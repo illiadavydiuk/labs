0a3047f8cae8f7d8ab4f3a9fc6caa3df44e3697e
Chicago <illay630@gmail.com>
2025-09-27 19:43:30 +0300

    first lab add skeleton

---
+++ lab1/first_task_file_encryptor/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace first_task_file_encryptor
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
