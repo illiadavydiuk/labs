58068cc9d9bc18c38ad26caeccf1ecfecee7f2ae
Illia Davydiuk <92821486+illiadavydiuk@users.noreply.github.com>
2025-11-14 21:26:25 +0200

    database connect + skeleton

---
+++ lab2/CarService.Core/Context/CarServiceDB.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarService.Core.Context
+{
+    public class CarServiceDB : DbContext
+    {
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                "Server=localhost,1433;Database=CarService;User Id=sa;Password=MyStr0ng!Passw0rd;TrustServerCertificate=True;",
+                sql => sql.MigrationsHistoryTable("__EFMigrationsHistory", "meta")
+            );
+        }
+    }
+}
