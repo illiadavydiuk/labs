c076637f9a40559984169c175cf4716ad267800d
Illia Davydiuk <92821486+illiadavydiuk@users.noreply.github.com>
2025-12-06 20:10:51 +0200

    add ui layer and now project .net 9.0

---
+++ DuckNet/DuckNet.UI/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace DuckNet.UI.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
