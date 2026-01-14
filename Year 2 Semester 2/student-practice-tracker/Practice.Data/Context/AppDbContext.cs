8b6a8a278200956e37331acbbe8b3137c17294b2
Chicago <92821486+illiadavydiuk@users.noreply.github.com>
2026-01-14 22:58:08 +0200

    add empty projects + connection settings + install packeges

---
+++ Year 2 Semester 2/student-practice-tracker/Practice.Data/Context/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice.Data.Context
+{
+    public class AppDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlite("Data Source=mydatabase.db");
+        }
+    }
+}
---
+++ Year 2 Semester 2/student-practice-tracker/README.md
@@ -0,0 +1,2 @@
+# student-practice-tracker
+Student practice tracker is a C# internship project developed using Avalonia/WPF and EF Core that automates the management of student internships through a system of roles (Admin, Supervisor, Student)
