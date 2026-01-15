0f96f408cf6da59ca169e30718ae659974844a35
Chicago <92821486+illiadavydiuk@users.noreply.github.com>
2026-01-16 01:10:00 +0200

    Add basic repository class and interface

---
+++ Year 2 Semester 2/student-practice-tracker/Practice.Repositories/Implementations/Repository.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Practice.Data.Context;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice.Repositories.Implementations
+{
+    public class Repository<T> where T : class
+    {
+        protected readonly AppDbContext _context;
+        protected readonly DbSet<T> _dbSet;
+
+        public Repository(AppDbContext context)
+        {
+            _context = context;
+            _dbSet = _context.Set<T>();
+        }
+        public virtual IEnumerable<T> GetAll() => _dbSet.ToList();
+
+        public virtual T GetById(int id) => _dbSet.Find(id);
+
+        public void Add(T entity) => _dbSet.Add(entity);
+
+        public void Update(T entity) => _dbSet.Update(entity);
+
+        public void Delete(int id)
+        {
+            var entity = GetById(id);
+            if (entity != null) _dbSet.Remove(entity);
+        }
+
+        public void Save() => _context.SaveChanges();
+    }
+}
---
+++ Year 2 Semester 2/student-practice-tracker/Practice.Repositories/Interfaces/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice.Repositories.Interfaces
+{
+    public interface IRepository<T> where T : class
+    {
+        IEnumerable<T> GetAll();
+        T GetById(int id);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+        void Save();
+    }
+}
